74005b516cb7c9230cba34aefd7555e6281b9d13
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-05-31 22:02:29 +0530

    Added code to print shortest common supersequence

--- Dynamic_Programming/LCS_problems/short_common_superseq.cpp
+++ Dynamic_Programming/LCS_problems/short_common_superseq.cpp
@@ -1,6 +1,51 @@
 #include "../../myHeaders.h"
 using namespace std;
 
+string rev(string an) {
+    string ans;
+    for(int i = an.length()-1; i >= 0; i--) {
+        ans += an[i];
+    }
+
+    return ans;
+}
+
+void print_scs(string s1, string s2, vector<vector<int>> &dp) {
+    int i = dp.size();
+    int j = dp[0].size();
+
+    string ans;
+
+    while(i > 0 && j > 0 ) {
+        if(s1[i-1] == s2[j-1]) {
+            ans += s1[i-1];
+            i--;
+            j--;
+        }
+        else {
+            if(dp[i-1][j] > dp[i][j-1]) {
+                ans += s1[i-1];
+                i--;
+            }
+            else {
+                ans += s2[j-1];
+                j--;
+            }
+        }
+    }
+    while(i > 0) {
+        ans += s1[i-1];
+        i -= 1;
+    }
+    while(j > 0) {
+        ans += s2[j-1];
+        j -= 1;
+    }
+    ans = rev(ans);
+
+    cout << "Shortest common superscript is : " << ans << endl;
+}
+
 void solve(string s1, string s2) {
     int n = s1.length();
     int m = s2.length();
@@ -21,6 +66,8 @@ void solve(string s1, string s2) {
     int ans = n + m - dp[n][m];
     
     cout << "Length of shorest common superscript : " << ans << endl;
+
+    print_scs(s1, s2, dp);
 }
 
 int main() {
