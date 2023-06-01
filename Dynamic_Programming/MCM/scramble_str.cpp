17741ac00d90b4a050bbdd09eaf27df3ba70a55a
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-06-01 15:14:52 +0530

    Added code for scrambled string in MCM

---
+++ Dynamic_Programming/MCM/scramble_str.cpp
@@ -0,0 +1,76 @@
+#include "../../myHeaders.h"
+using namespace std;
+
+bool check(string s1, string s2, int i, int j) {
+    bool aa = true;
+    bool bb = true;
+    
+    for(int k = i; k <= j; k++) {
+        if(s1[k] != s2[k]) {
+            aa = false;
+        }
+        if(s1[k] != s2[j-k+i]){
+            bb = false;
+        }
+    }
+
+    return aa | bb;
+}
+
+bool mcm(string s1, string s2, int i, int j) {
+    
+    if(i >= j) {
+        if(s1[i] == s2[i]) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if(check(s1, s2, i, j)) {
+        return true;
+    }
+
+    bool ans = false;
+    for(int k = i; k < j; k++) {
+        bool aa = mcm(s1, s2, i, k);
+        bool bb = mcm(s1, s2, k+1, j);
+
+        ans = aa&bb;
+        if(ans == true) break;
+    }
+
+    return ans;
+}
+
+void solve(string s1, string s2) {
+    if(s1.length() != s2.length()) {
+        cout << "s2 is not the scrambled string of s1" << endl;
+    }
+
+    bool ans = mcm(s1, s2, 0, s1.length()-1);
+    
+    if(ans) {
+        cout << "s2 is scrambled string of s1" << endl;
+    }
+    else {
+        cout << "s2 is not scrambled string of s1" << endl;
+    }
+}
+
+int main() {
+    string s1, s2; cin >> s1 >> s2;
+    solve(s1, s2);
+}
+
+/*
+phqtrnilf
+ilthnqrpf
+
+rgtae
+great
+
+we 
+we
+*/
